76e2e49873bf19ad68ed7b4012738d4c85050d8b
Aaryan Singh <singhaaryan376@gmail.com>
2024-01-04 12:14:39 +0530

    Updated the code to remove any anomaly

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -55,10 +55,10 @@ class Graph extends Component<IProps, {}> {
       elem.setAttribute('columns','["top_ask_price"]');
       elem.setAttribute('aggregates',`
        {
-        "stock": "distinct count",
+        "stock": "distinct_count",
         "top_ask_price": "avg",
         "top_bid_price": "avg",
-        "timestamp": "distinct count"
+        "timestamp": "distinct_count"
        }`);
     }   
   }
